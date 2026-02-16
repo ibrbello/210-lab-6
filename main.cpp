38ed370bf0f737ea534bfa546a7b2822bba7bb7c
Ibrahim Bello <ibrbello10@gmail.com>
2026-02-16 14:37:44 -0800

    Completed Lab 6 (apologies for late submission)

--- main.cpp
+++ main.cpp
@@ -11,8 +11,9 @@ double sumArray(double*);
 int main(){
     double *iptr = new double[SIZE];
     enterArrayData(iptr);
-    //outputArrayData(iptr);
-    //double total = sumArray(iptr);
+    outputArrayData(iptr);
+    double total = sumArray(iptr);
+    cout << "Sum of values: " << total;
     return 0;
 }
 // This function receives an  array and 
@@ -22,8 +23,8 @@ int main(){
 void enterArrayData(double* arr){
     cout << "Data entry for the array: " << endl;
     for (int i = 0; i < SIZE; i++ ){
-        cout << "Element #" << (i+1) << ": ";
-        cin >> arr[i];
+        cout << "   > Element #" << (i+1) << ": ";
+        cin >> *(arr + i);
     }
     cout << "Data entry complete." << endl;
 }
@@ -32,5 +33,22 @@ void enterArrayData(double* arr){
 // Arguments: Dynamic double array to hold floats
 // Returns: None
 void outputArrayData(double* arr){
-    
+    cout << "Outputting array elements: ";
+    for (int i = 0; i < SIZE; i++){
+        cout << *(arr + i) << " ";
+    }
+    cout << endl;
+    cout << "Output complete." << endl;
+}
+
+// This function receives an array and calculates and returns
+// its result.
+// Arguments: Dynamic double array to hold floats
+// Returns: Double holding sum of elements in array
+double sumArray(double* arr){
+    double total = 0;
+    for (int i = 0; i < SIZE; i++){
+        total += *(arr + i);
+    }
+    return total;
 }
